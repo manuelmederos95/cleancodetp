f2b9edc32b77687e51fe2d1d35e31fdd9f4f21c2
MEDEROS Manuel <manuel.mederos95@gmail.com>
2017-10-03 10:41:06 +0200

    first commit

---
+++ Bureau/TP1.py
@@ -0,0 +1,12 @@
+import csv
+import json
+import time
+
+def function(name):
+	x = open(name + '.json').read()
+	x = json.loads(x)
+	f = csv.writer(open("name-" + time.strftime('%Y-%m-%d %H:%M:%S') + ".csv", "wb+"))
+
+
+	for x in x["list"]:
+		f.writerow([x["host"], x["path"], x["code"], x["durationMs"]])
