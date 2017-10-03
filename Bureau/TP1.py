125e270c8da7ce73fb4e7c2e963b214946246882
manuelmederos95 <32478863+manuelmederos95@users.noreply.github.com>
2017-10-03 11:18:54 +0200

    Update TP1.py

--- Bureau/TP1.py
+++ Bureau/TP1.py
@@ -5,7 +5,7 @@ import time
 def function(name):
 	x = open(name + '.json').read()
 	x = json.loads(x)
-	f = csv.writer(open("name-" + time.strftime('%Y-%m-%d %H:%M:%S') + ".csv", "wb+"))
+	f = csv.writer(open(name + "-" + time.strftime('%Y-%m-%d %H:%M:%S') + ".csv", "wb+"))
 
 
 	for x in x["list"]:
